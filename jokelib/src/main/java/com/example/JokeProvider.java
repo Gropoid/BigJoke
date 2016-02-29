17fe926f59966a79f9ffe1f77b3ba550ae3a7d35
geraud <geraud.floodme@gmail.com>
2016-02-29 17:41:20 +0100

    Add JokeProvider class in the jokelib Add dependency to it in app MADE IT "sourceCompatibility = 1.7" !

---
+++ jokelib/src/main/java/com/example/JokeProvider.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class JokeProvider {
+    public static String getJoke() {
+        return "How do you find Will Smith in the snow? You look for the fresh prints";
+    }
+}
